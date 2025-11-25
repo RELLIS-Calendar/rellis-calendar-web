b0072bde7b06a6e718977facaa5eab845024a728
iDrannik <bratgamerz@gmail.com>
2025-11-24 22:48:06 -0600

    Added navbar, pages, edited idex to fix color flashing on page switch, installed router dom

---
+++ src/pages/Calendar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Calendar.css";
+
+function Calendar() {
+  return (
+    <div className="calendar-container">
+      <h1>home page</h1>
+
+    </div>
+  );
+}
+
+export default Calendar;
---
+++ src/pages/Profile.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./profile.css";
+
+function profile() {
+  return (
+    <div className="profile-container">
+      <h1>profile page</h1>
+
+    </div>
+  );
+}
+
+export default profile;
