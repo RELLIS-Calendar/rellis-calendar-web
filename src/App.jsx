477cae3c462141f0428bad20b46d073a24a2b84e
CyberGaI <hope.kalina13@gmail.com>
2025-12-03 09:12:34 -0600

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import Calendar from "./Calendar";
+
+export default function App() {
+    return (
+        <div>
+            <h1>Calendar Demo</h1>
+            <Calendar />
+        </div>
+    );
+}
